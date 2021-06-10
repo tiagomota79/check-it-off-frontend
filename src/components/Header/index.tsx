2c43df9d2dedc8f460f8b563c31de55e49eb18d4
tiagomota79 <tiagomot@gmail.com>
2021-06-10 17:59:46 -0400

    Created simple Header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { HeaderContainer, Logo } from './styles';
+
+const Header: React.FC = () => {
+  return (
+    <HeaderContainer>
+      <div />
+      <Logo>Check It Off!</Logo>
+      <div>Night</div>
+      <div>Lang</div>
+    </HeaderContainer>
+  );
+};
+
+export default Header;
---
+++ src/components/Header/styles.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const HeaderContainer = styled.div`
+  display: grid;
+  grid-template-columns: 100px auto 50px 50px;
+  place-items: center;
+  width: 100%;
+`;
+
+export const Logo = styled.div`
+  font-family: MontserratBlack;
+  font-size: 42px;
+`;
