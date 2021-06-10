a7e85f30e7ac892ab229196ec3f2ab3c7343fbda
tiagomota79 <tiagomot@gmail.com>
2021-06-10 17:10:47 -0400

    Split iToDoList into two state interfaces

--- src/type.d.ts
+++ src/type.d.ts
@@ -1,9 +1,19 @@
+interface ITask {
+  text: string;
+  active: boolean;
+  listIndex: number;
+}
+
 interface IList {
   name: string;
   description: string;
   active: boolean;
 }
 
-interface IToDoLists {
+interface IListsState {
   lists: IList[];
+}
+
+interface ITasksState {
+  tasks: ITask[];
 }
