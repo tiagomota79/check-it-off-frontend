e765ed4941d9ab86dae445d7f0c64456c34950d3
tiagomota79 <tiagomot@gmail.com>
2021-06-10 22:19:49 -0400

    Removed unused import

--- src/components/Task/index.tsx
+++ src/components/Task/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import { useAppDispatch, useAppSelector } from '../../hooks/hooks';
 import { deleteTask, selectTasks } from '../../slices/tasksSlice';
