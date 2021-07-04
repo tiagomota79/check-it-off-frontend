03a5bd726a78b9e8efd7f52cc5d896533d5ab789
tiagomota79 <tiagomot@gmail.com>
2021-07-04 09:07:02 -0400

    Added theme reducer to store

--- src/store.ts
+++ src/store.ts
@@ -1,11 +1,13 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import listsReducer from './slices/listsSlice';
 import tasksReducer from './slices/tasksSlice';
+import themeReducer from './slices/themeSlice';
 
 export const store = configureStore({
   reducer: {
     lists: listsReducer,
     tasks: tasksReducer,
+    theme: themeReducer,
   },
 });
 
